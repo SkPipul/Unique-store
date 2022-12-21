15ab9f597667f499cac19792c198780b78e0d5d2
SkPipul <skjabed252@gmail.com>
2022-12-21 21:26:03 +0600

    design added

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -5,10 +5,10 @@ const Banner = () => {
     <div className="mt-8 mb-20">
         <h2 className="text-center text-3xl font-mono font font-extrabold my-2">You Can Sell or Buy Mobile from our website</h2>
         <p className="text-center">We are provide many kinds of used phones from seller and we buy this to the customer through on our website.</p>
-      <div className="carousel w-1/2 mx-auto mb-10 mt-5 rounded-sm">
+      <div className="carousel w-3/4 mx-auto mb-10 mt-5 rounded-sm">
         <div id="slide1" className="carousel-item relative w-full">
           <img
-            src="https://images.unsplash.com/photo-1584438784894-089d6a62b8fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+            src="https://i.ibb.co/K9mxhbJ/Apogaeis-6-Biggest-Challenges-in-Mobile-App-Development.jpg"
             className="w-full"
             alt=""
           />
@@ -23,7 +23,7 @@ const Banner = () => {
         </div>
         <div id="slide2" className="carousel-item relative w-full">
           <img
-            src="https://plus.unsplash.com/premium_photo-1664197368374-605ce8ec8f54?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1vYmlsZSUyMHBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            src="https://i.ibb.co/JCpxL6J/Tw-G-UK-Mastering-User-Acquisition-HERO-width-1200.jpg"
             className="w-full"
             alt=""
           />
@@ -38,7 +38,7 @@ const Banner = () => {
         </div>
         <div id="slide3" className="carousel-item relative w-full">
           <img
-            src="https://images.unsplash.com/photo-1585060282215-39a72f82385c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fG1vYmlsZSUyMHBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            src="https://i.ibb.co/YZMhSMH/mobile-apps-development-nz-1-modified.jpg"
             className="w-full"
             alt=""
           />
@@ -53,7 +53,7 @@ const Banner = () => {
         </div>
         <div id="slide4" className="carousel-item relative w-full">
           <img
-            src="https://images.unsplash.com/photo-1544725121-be3bf52e2dc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fG1vYmlsZSUyMHBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            src="https://i.ibb.co/z5qqqcR/userandmobile-jpg-e1455151356623.jpg"
             className="w-full"
             alt=""
           />
