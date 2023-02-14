437449f48b54774348d0e722a4e042481a03eca8
SkPipul <skjabed252@gmail.com>
2023-02-14 13:40:04 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import router from './Routes/Routes/Routes';
-import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
   return (
     <div className="">
       <RouterProvider router={router}></RouterProvider>
-      <div>
-        
-      <MessengerCustomerChat
-        pageId="100083158557902"
-        appId="914690736626725"
-      />
-      </div>
     </div>
+    
   );
 }
 
